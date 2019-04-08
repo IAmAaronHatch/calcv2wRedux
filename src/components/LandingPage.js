52f60558d85f1b21f0b447752cd6c2195f860243
Aaron Hatch <aaron@Aarons-MBP.attlocal.net>
2019-04-07 19:24:46 -0700

    first commit, added LandingPage

---
+++ src/components/LandingPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class LandingPage extends Compoennt {
+    render () {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default LandingPage
